304c36171d03b0d1014eeca3d84e905c302deb7c
afigueras1987 <afigueras1987@gmail.com>
2018-12-19 19:12:38 -0600

    mensaje

--- js/permissions.js
+++ js/permissions.js
@@ -1,14 +1,16 @@
 
 (function () {
 
-	$(document).ready(function() {
-		var showOptionsByMenu  = function(permissions){
-			var permission_list = $.map(permissions, function(elm) { return elm });
+	$(document).ready(function () {
+		var showOptionsByMenu = function (permissions) {
+			//var permission_list = $.map(permissions, function(elm) { return elm });
+			var permission_list = ['index-navvar-search', 'index-reports-content', 'clients-range-reports']
 			//console.info(permission_list);
 			permission_list.forEach((val, key)=>{
 				$(`[permission-route=${val}]`).css('display', 'inline-block');
 				$(`[permission-route=${val}]`).removeAttr('hidden');
 			})
+<<<<<<< Updated upstream
 		};
 
 	 	$.ajax({
@@ -24,6 +26,35 @@
 	      	}
 
       	});  	
+=======
+		
+		}
+
+			$.ajax({
+				   url: "./security/check_auth.php",      
+			 })
+			 .done(function(response) {      
+				   // console.info(response);
+				   let data = JSON.parse(response);
+				   if(data.status === 401){
+					   window.location.href = "index.html";	      		
+				   }else if(data.status === 200){
+					   showOptionsByMenu(data.permissions);
+				   }
+   
+			   });
+
+  	/* este dejar asi let auth_email_token = "ba845c00-839a-43ab-8c71-af07ee4f2276";
+	    Email.send({
+	    SecureToken : auth_email_token,
+	    To : 'juanramon89@gmail.com',
+	    From : "ramonlahabana1989@gmail.com",
+	    Subject : "test email from js",
+	    Body : "todo ok"
+	}).then(
+	  message => alert(message)
+	  );*/
+>>>>>>> Stashed changes
 	});
 
 })();
