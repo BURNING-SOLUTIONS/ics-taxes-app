664e58f63566c69b1e44bc9b1cea803de8be2aac
Ramon <ramon.vidala89@gmail.com>
2023-11-06 23:11:46 +0100

    reparando error en las tarifas de carga

--- security/login.php
+++ security/login.php
@@ -5,12 +5,17 @@
  	$PROYECT_CONFIG = parse_ini_file('../config/config.ini');
 	$today = new DateTime(); # today date
 
-	$sqlConection = new sqlServerConecction(
-	    "$PROYECT_CONFIG[database_host]".", "."$PROYECT_CONFIG[database_port]",
-	    "dl" . $today->format('Y') /* dl2018 */,
-	    "$PROYECT_CONFIG[database_user]",
-	    "$PROYECT_CONFIG[database_password]"
-	);
+	try{
+		$sqlConection = new sqlServerConecction(
+			"$PROYECT_CONFIG[database_host]".", "."$PROYECT_CONFIG[database_port]",
+			"dl" . $today->format('Y') /* dl2018 */,
+			"$PROYECT_CONFIG[database_user]",
+			"$PROYECT_CONFIG[database_password]"
+		);
+	}catch(Exception $ex){
+		echo $ex->getMessage();
+		exit();
+	}
 
     # consulta numero 1..
 	$getusuarios = $sqlConection->createQuery("SELECT Nom_Usu,Password_Usu FROM dbo.usuarios 
@@ -27,18 +32,3 @@
 		header("location: ../index.html");
 		exit;
 	}
-	
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
